90a18991b715945a5c0288c9c013849140b34daa
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-12-12 16:33:42 +0700

    Add project files.

---
+++ Queues_045/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues_045
+{
+    class Program
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        static void Main(string[] args)
+        {
+        }
+    }
+}
