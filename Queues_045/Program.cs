0020b3307ab9ecb927e5d2f608d98a978f186407
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2022-12-12 16:42:23 +0700

    membuat public void insert

--- Queues_045/Program.cs
+++ Queues_045/Program.cs
@@ -10,6 +10,22 @@ namespace Queues_045
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+
+        public Program()
+        {
+            /*initializing the values of the variables REAR and FRONT to -1 to indicate that 
+             * the name is initially empty,*/
+            FRONT = -1;
+            REAR = -1;
+        }
+        public void insert(int element)
+        {
+            /*This statement check for the overflow condition. */
+            if ((FRONT == 0 && REAR == max - 1 ) || (FRONT == REAR +1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+            }
+        }
         static void Main(string[] args)
         {
         }
